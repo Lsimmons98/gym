f497aa2cc9ee87dd67a822b70a5eaa1ecec32814
Liam Simmons <liam.simmons@powerhrg.com>
2024-05-28 16:00:23 -0400

    Complete first story to search exercises by muscle group.

--- main.js
+++ main.js
@@ -1,1 +1,48 @@
-//
+const exerciseForm = document.getElementById('exercise-form')
+const searchExercise = document.getElementById('exercise').value
+const mainExerciseList = document.getElementById('main-exercise-list')
+const searchExerciseList = document.getElementById('search-exercise-list')
+const searchTitle = document.getElementById('muscle-group-title')
+let exerciseList = []
+
+
+const retrieveAllExercises = () => {
+  fetch('http://localhost:3000/exercises')
+  .then(resp => resp.json())
+  .then(resp => {
+    resp.forEach(exercise => {
+      exerciseToDom(exercise)
+    })
+  })
+}
+
+const exerciseToDom = (exercise) => {
+  const createListItem = document.createElement('li')
+  createListItem.textContent = `${exercise.exercise_name}`
+
+  exerciseList.push(exercise)
+  mainExerciseList.appendChild(createListItem)
+}
+
+retrieveAllExercises()
+
+
+const filterExercises = (array) => {
+  const searchExercise = document.getElementById('exercise').value
+
+  searchTitle.textContent = `${searchExercise} Exercises`
+  searchExerciseList.innerHTML = ''
+
+  const searchList = array.filter(exercise => exercise.muscle_group === searchExercise)
+  searchList.forEach(exercise => {
+    const createListItem = document.createElement('li')
+    createListItem.textContent = `${exercise.exercise_name}`
+
+    searchExerciseList.appendChild(createListItem)
+  })
+}
+
+exerciseForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+  filterExercises(exerciseList)
+})
